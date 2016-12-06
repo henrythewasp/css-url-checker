4e667814c60716db74820dd2ab7869fecb48f274
Gary Taylor <henrythewasp@gmail.com>
2016-12-06 17:45:26 +0000

    A working async version!!! :-)

--- tasks/css_img_checker.js
+++ tasks/css_img_checker.js
@@ -19,105 +19,58 @@ module.exports = function(grunt) {
 		var webroot = this.options().webroot || '';
 		var done = this.async();
 
-		var mylist = [
-			'list1', 'list2', 'list3'
-		];
-
-		var myfiles = [
-			'file1', 'file2', 'file3'
-		];
-		var urls = [ 
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/petition.png',
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/content.png',
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/datastore.png',
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/group.png',
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/task.png',
-			'http://gtconsult.inovem.com/inovem/sites/images/objects/small/report.png'
-		];
-
 		grunt.log.writeln('Webroot: ' + webroot);
 
-		async.forEach(mylist, function(e, nextEntry) {
-			grunt.log.writeln('Checking files in listentry: ' + e);
-
-			async.forEach(myfiles, function(f, nextFile) {
-				grunt.log.writeln('Checking URLs in file: ' + f);
-
-				async.forEach(urls, function(u, nextURL) {
-				/*	
-					if (e === 'list3' && f === 'file2' && u === 'http://gtconsult.inovem.com/inovem/sites/images/objects/small/group.png') {
-						u = 'http://gt.inovem.com/bollox.gif';
-					}
-				*/
-					
-					http.verify({
-						url: u,
-						conditions: {
-							type: 'statusCode',
-						value: 200
-						}
-					}, function(err) {
-						if (err === null) {
-							grunt.log.writeln('URL ' + u + ' checked');
-							nextURL();
-						} else {
-							grunt.log.error('URL ' + u + ' is bad, err: ' + err);
-							nextURL(err);
-						}
-					});
-				}, function(error) {
-					grunt.log.writeln('URLS FINISHED! Error is: ' + error);
-					if (error === null) {
-						grunt.log.writeln('All URLS checked in file ' + f);
-						nextFile();
-					} else {
-						nextFile(error);
-					}
-				});
-			}, function(error) {
-				grunt.log.writeln('FILES FINISHED! Error is: ' + error);
-				if (error === null) {
-					grunt.log.writeln('All FILES checked');
-					nextEntry();
-				} else {
-					nextEntry(error);
-				}
-			});
-
-		}, function(error) {
-			grunt.log.writeln('LIST FINISHED! Error is: ' + error);
-			if (error === null) {
-				grunt.log.writeln('All ENTRIES OK');
-				done();
-			} else {
-				done(error);
-			}
-		});
-
-		/*
-		this.files.forEach(function(file) {
+		async.forEach(this.files, function(file, nextEntry) {
 			grunt.log.writeln('Processing: ' + file.src.length + ' files');
 
-			file.src.forEach(function(f) {
-				grunt.log.writeln('Checking ' + f);
+			async.forEach(file.src, function(f, nextFile) {
+				grunt.log.writeln('Checking URLs in file: ' + f);
 				var urls = parser(grunt.file.read(f));
-				urls.forEach(function(u) {
-					if (!u.toLowerCase().startsWith('http') && !u.toLowerCase().startsWith('data:')) {
+
+				async.forEach(urls, function(u, nextURL) {
+					if (u.toLowerCase().startsWith('http')) {
+						http.verify({
+							url: u,
+							conditions: {
+								type: 'statusCode',
+								value: 200
+							}
+						}, function(err) {
+							if (err !== null) {
+								grunt.log.error('Bad URL [' + u + '] in file [' + f + '] Error: ' + err);
+							}
+							nextURL(err);
+						});
+
+					} else if (!u.toLowerCase().startsWith('data:')) {
 						u = path.join(webroot, u);
+						var err = null;
 
 						if (!grunt.file.exists(u)) {
-							grunt.log.error('Missing: ' + u);
+							err = new Error('Missing file!');
+							grunt.log.error('Bad URL [' + u + '] in file [' + f + '] Error: ' + err);
 						}
+						nextURL(err);
 					}
+
+				}, function(err) {
+					nextFile(err);
 				});
 
+			}, function(err) {
+				nextEntry(err);
 			});
+
+		}, function(err) {
+			if (err === null) {
+				grunt.log.writeln('All files checked: OK');
+			} else {
+				grunt.log.error('All files checked: ' + err);
+			}
+			done(err);
 		});
 
-		if (this.errorCount) { return false; }
-		*/
-
-		// grunt.log.writeln('All CSS files checked OK');
 	});
 
 };
